7ca41a1e0e12b99f19122eb1d6cc4725cb90f331
RyuuDev-12 <88930646+RyuuDev-12@users.noreply.github.com>
2023-08-03 18:42:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const image = document.querySelector('.main-img')
+const body = document.querySelector('body')
 
-console.log(image)
+function interacaoImg(event){
+    image.style.right = 'calc(5vw + ' + event.pageX + 'rem/400)'
+    console.log(event.pageX)
+}
+
+body.addEventListener('mousemove', function(event){
+    interacaoImg(event)
+})
